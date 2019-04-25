216e186ccd69222ff913c68a2062b498535c2f5f
Andrea F. Daniele <afdaniele@ttic.edu>
2019-04-24 21:52:06 -0700

    minor

--- modules/renderers/blocks/SensorMsgs_CompressedImage.php
+++ modules/renderers/blocks/SensorMsgs_CompressedImage.php
@@ -41,7 +41,7 @@ class SensorMsgs_CompressedImage extends BlockRenderer{
       ]
     ],
     "style" => [
-      "name" => "Image distortion",
+      "name" => "Image size",
       "type" => "enum",
       "mandatory" => True,
       "default" => "cover",
@@ -54,60 +54,60 @@ class SensorMsgs_CompressedImage extends BlockRenderer{
       "name" => "Background color",
       "type" => "color",
       "mandatory" => True,
-      "default" => "#000"
+      "default" => "#fff"
     ]
   ];
 
-  protected static function render( $id, &$args ){
+  protected static function render($id, &$args){
     ?>
     <div id="image_placeholder"></div>
 
     <script type="text/javascript">
-    $( document ).on("<?php echo ROS::$ROSBRIDGE_CONNECTED ?>", function(evt){
-      // Subscribe to the CompressedImage topic
-      subscriber = new ROSLIB.Topic({
-        ros : window.ros,
-        name : '<?php echo $args['topic'] ?>',
-        messageType : 'sensor_msgs/CompressedImage',
-        queue_size : 1,
-        throttle_rate : <?php echo intval(1000/$args['fps']) ?>
-      });
+      $(document).on("<?php echo ROS::$ROSBRIDGE_CONNECTED ?>", function(evt){
+        // Subscribe to the CompressedImage topic
+        subscriber = new ROSLIB.Topic({
+          ros : window.ros,
+          name : '<?php echo $args['topic'] ?>',
+          messageType : 'sensor_msgs/CompressedImage',
+          queue_size : 1,
+          throttle_rate : <?php echo intval(1000/$args['fps']) ?>
+        });
 
-      subscriber.subscribe(function(message) {
-        canvas = $('#<?php echo $id ?>');
-        base64_string = 'data:image/jpg;base64,'+message['data'];
-        canvas.css('background-image', 'url(' + base64_string + ')');
-        // hide placeholder
-        $('#<?php echo $id ?> #image_placeholder').css('display', 'none');
-        // refresh style
-        $('#<?php echo $id ?>').css('background-position', '<?php echo $args['position'] ?>');
-        $('#<?php echo $id ?>').css('background-size', '<?php echo $args['style'] ?>');
+        subscriber.subscribe(function(message) {
+          canvas = $('#<?php echo $id ?>');
+          base64_string = 'data:image/jpg;base64,'+message['data'];
+          canvas.css('background-image', 'url(' + base64_string + ')');
+          // hide placeholder
+          $('#<?php echo $id ?> #image_placeholder').css('display', 'none');
+          // refresh style
+          $('#<?php echo $id ?>').css('background-position', '<?php echo $args['position'] ?>');
+          $('#<?php echo $id ?>').css('background-size', '<?php echo $args['style'] ?>');
+        });
       });
-    });
     </script>
 
     <style type="text/css">
-    #<?php echo $id ?>{
-      background-color: <?php echo $args['background_color'] ?>;
-      background-position: <?php echo $args['position'] ?>;
-      background-size: <?php echo $args['style'] ?>;
-      background-repeat: no-repeat;
-    }
+      #<?php echo $id ?>{
+        background-color: <?php echo $args['background_color'] ?>;
+        background-position: <?php echo $args['position'] ?>;
+        background-size: <?php echo $args['style'] ?>;
+        background-repeat: no-repeat;
+      }
 
-    #<?php echo $id ?> .block_renderer_header{
-      background-color:rgba(0, 0, 0, 0.3);
-      color: white;
-    }
+      #<?php echo $id ?> .block_renderer_header{
+        background-color:rgba(0, 0, 0, 0.1);
+        color: white;
+      }
 
-    #<?php echo $id ?> #image_placeholder{
-      width:100%;
-      height:100%;
-      max-height: 100px;
-      background-image: url('<?php echo Core::getImageURL('placeholder.png') ?>');
-      background-position: center center;
-      background-size: auto 100%;
-      background-repeat: no-repeat;
-    }
+      #<?php echo $id ?> #image_placeholder{
+        width:100%;
+        height:100%;
+        max-height: 100px;
+        background-image: url('<?php echo Core::getImageURL('placeholder.png') ?>');
+        background-position: center center;
+        background-size: auto 100%;
+        background-repeat: no-repeat;
+      }
     </style>
     <?php
   }//render
